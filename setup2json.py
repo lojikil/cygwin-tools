d932c5562513f7aa3a7c1a00df0eee95423beadd
Stefan Edwards <saedwards.ecc@gmail.com>
2012-09-22 00:34:31 -0400

    initial commit

---
+++ setup2json.py
@@ -0,0 +1,61 @@
+import json
+import sys
+
+# Giant hack to convert Cygwin's "setup" file to a JSON
+# file, you know, so someone can use it for something with a
+# reasonable amount of work :|
+
+if len(sys.argv) > 1:
+    setupfile = file(sys.argv[1], 'r')
+else:
+    setupfile = file('setup', 'r')
+
+packages = {}
+pkg_name = None
+pkg = None
+prev_flag = False
+for line in setupfile:
+    line = line[0:-1]
+    print line
+    if '@ ' == line[0:2]:
+        pkg_name = line[2:]
+        packages[pkg_name] = {}
+        pkg = packages[pkg_name]
+        prev_flag = False
+    elif '# ' == line [0:2]:
+        continue
+    elif 'setup-' == line[0:7]:
+        continue
+    elif 'sdesc' in line:
+        pkg['sdesc'] = line[8:]
+    elif 'ldesc' in line:
+        ldesc = [line[9:]]
+        for nu_line in setupfile:
+            ldesc.append(nu_line)
+            if '"' in nu_line:
+                break
+        pkg['ldesc'] = ''.join(ldesc)
+    elif 'category:' in line:
+        pkg['category'] = line[10:]
+    elif 'requires:' in line:
+        pkg['requires'] = line[10:].split(' ')
+    elif '[prev]' in line:
+        prev_flag = True
+    elif 'version:' in line[0:9]:
+        if prev_flag:
+            pkg['prev_version'] = line[9:]
+        else:
+            pkg['version'] = line[9:]
+    elif 'install:' in line:
+        if prev_flag:
+            pkg['prev_install'] = line[9:].split(' ')
+        else:
+            pkg['install'] = line[9:].split(' ')
+    elif 'source:' in line:
+        if prev_flag:
+            pkg['prev_source'] = line[8:].split(' ')
+        else:
+            pkg['source'] = line[8:].split(' ')
+
+
+print json.dumps(packages)
